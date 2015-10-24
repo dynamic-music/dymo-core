1d05dea9541bedbcc859fd159c74d9a767faff09
Florian Thalmann <flo@flos-macbook.home>
2015-10-24 19:04:32 +0100

    added some reverb to the tests

--- spec/dymoloader-spec.js
+++ spec/dymoloader-spec.js
@@ -6,6 +6,7 @@ describe("a dymoloader", function() {
 	var scheduler, dymo, dymoMap, rendering;
 	var dymoPath = '../example/dymo.json';
 	var renderingPath = '../example/rendering.json';
+	var reverbPath = '../example/impulse_rev.wav';
 	
 	it("loads a dymo from json", function(done) {
 		scheduler = new Scheduler(audioContext, function(num) {
@@ -16,6 +17,7 @@ describe("a dymoloader", function() {
 				done();
 			}, 2000);
 		});
+		scheduler.setReverbFile(reverbPath);
 		var loader = new DymoLoader(scheduler);
 		loader.loadDymoFromJson(dymoPath, function(loadedDymo) {
 			dymo = loadedDymo[0];
@@ -30,7 +32,7 @@ describe("a dymoloader", function() {
 		var loader = new DymoLoader(scheduler);
 		loader.loadRenderingFromJson(renderingPath, dymoMap, function(loadedRendering) {
 			rendering = loadedRendering;
-			expect(rendering.getMappings().length).toEqual(2);
+			expect(rendering.getMappings().length).toEqual(3);
 			scheduler.play(dymo);
 			setTimeout(function() {
 				//expect(scheduler.urisOfPlayingDmos).toEqual(["dymo1"]);
