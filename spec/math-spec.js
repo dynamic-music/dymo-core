ad59023d412f280ebca8e7c1a2696504bbb4d0be
Florian Thalmann <flo@lt91-58.eecs.qmul.ac.uk>
2015-12-14 10:30:15 +0000

    added polygon tools which can return functions that determine whetther a point is in a specific polygon, how far it is from its edge, and how close to the centroid. added tests and they work

--- spec/math-spec.js
+++ spec/math-spec.js
@@ -26,4 +26,43 @@ describe("the math tools", function() {
 		expect(inversion).toBeUndefined();
 	});
 	
+	it("can determine whether a point is in a polygon", function() {
+		var polygon = [{0:0,1:0}, {0:5,1:1}, {0:3,1:3}];
+		var point1 = {0:3,1:2}; //inside
+		var point2 = {0:3,1:0}; //outside
+		var point3 = {0:4,1:2}; //on the line
+		expect(PolygonTools.isPointInPolygon(polygon, point1)).toBeTruthy();
+		expect(PolygonTools.isPointInPolygon(polygon, point2)).toBeFalsy();
+		expect(PolygonTools.isPointInPolygon(polygon, point3)).toBeFalsy();
+	});
+	
+	it("can find the centroid of a polygon", function() {
+		var polygon = [{0:0,1:0}, {0:5,1:2}, {0:4,1:4}];
+		expect(PolygonTools.getCentroid(polygon)).toEqual({0:3,1:2});
+	});
+	
+	it("can find the minimum distance from an edge of a polygon", function() {
+		var polygon = [{0:0,1:0}, {0:5,1:1}, {0:3,1:3}];
+		var point1 = {0:3,1:2}; //inside
+		var point2 = {0:2,1:1}; //inside
+		var point3 = {0:4,1:2}; //on the line
+		var centroid = PolygonTools.getCentroid(polygon);
+		expect(PolygonTools.getMinDistanceFromEdge(polygon, point1)).toBeCloseTo(0.7071067811865476, 15);
+		expect(PolygonTools.getMinDistanceFromEdge(polygon, centroid)).toBe(0.7844645405527361, 15);
+		expect(PolygonTools.getMinDistanceFromEdge(polygon, point2)).toBe(0.588348405414552, 15);
+		expect(PolygonTools.getMinDistanceFromEdge(polygon, point3)).toBe(0);
+	});
+	
+	it("can determine how far a point is in a polygon", function() {
+		var polygon = [{0:0,1:0}, {0:5,1:1}, {0:3,1:3}];
+		var point1 = {0:3,1:2}; //inside
+		var point2 = {0:3,1:0}; //outside
+		var point3 = {0:4,1:2}; //on the line
+		var centroid = PolygonTools.getCentroid(polygon);
+		expect(PolygonTools.howFarIsPointInPolygon(polygon, centroid, point1)).toBeCloseTo(0.352092461065317, 15);
+		expect(PolygonTools.howFarIsPointInPolygon(polygon, centroid, centroid)).toBe(1);
+		expect(PolygonTools.howFarIsPointInPolygon(polygon, centroid, point2)).toBe(0);
+		expect(PolygonTools.howFarIsPointInPolygon(polygon, centroid, point3)).toBe(0);
+	});
+	
 });
