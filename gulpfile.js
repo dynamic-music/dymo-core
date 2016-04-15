bcff43b3aae72143dd41a7313e429d97c9e56e94
Florian Thalmann <flo@Flos-MacBook.local>
2016-04-15 18:14:47 +0100

    tried closure compiler with advanced optimizations, all exported code would have to be rewritten with prototypes, maybe later

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ var gulp = require('gulp');
 var closureCompiler = require('google-closure-compiler').gulp();
 
 gulp.task('default', function () {
+	//return gulp.src(['./node_modules/google-closure-library/closure/goog/base.js', './src/**/*.js', './lib/soundtouch-js/soundtouch-min.js'], {base: './'})
 	return gulp.src(['./src/**/*.js', './lib/soundtouch-js/soundtouch-min.js'], {base: './'})
 	.pipe(closureCompiler({
-		compilation_level: 'ADVANCED_OPTIMIZATIONS',
+		//compilation_level: 'ADVANCED_OPTIMIZATIONS',
+		compilation_level: 'SIMPLE_OPTIMIZATIONS',
 		warning_level: 'VERBOSE',
 		externs: './externs.js',
+		//generate_exports: true,
 		language_in: 'ECMASCRIPT6',
 		language_out: 'ECMASCRIPT5',
 		js_output_file: 'dymo-core.min.js'
