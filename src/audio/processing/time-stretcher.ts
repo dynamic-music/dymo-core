0427978868030c7bfe2f35141144605284efef96
Lucas Thompson <dev@lucas.im>
2017-10-12 15:52:38 +0100

    Simplify timestretch base class, and inject stretcher into AudioProcessor

--- src/audio/processing/time-stretcher.ts
+++ src/audio/processing/time-stretcher.ts
@@ -1,11 +1,4 @@
 export abstract class TimeStretcher {
-  
-	private audioContext;
-
-	constructor(audioContext) {
-		this.audioContext = audioContext;
-	}
-
 	abstract timeStretch(sourceBuffer: AudioBuffer, goalBuffer: AudioBuffer, ratio: number): void;
 
 }
